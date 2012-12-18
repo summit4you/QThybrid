f2ca4655862e450ef2fd2bdb3fb13da530e7a6df
summit4you <summit_mail@qq.com>
2012-12-18 23:09:47 +0800

    增加操作系统页面模拟例子

---
+++ examples/pfnapp/htm/js/example.js
@@ -0,0 +1,7 @@
+// Run the script on DOM ready:
+$(function(){
+ $('table').visualize({type: 'pie', height: '1024px', width: '1024px'});
+	$('table').visualize({type: 'bar', width: '1024px'});
+	$('table').visualize({type: 'area', width: '1024px'});
+	$('table').visualize({type: 'line', width: '1024px'});
+});
